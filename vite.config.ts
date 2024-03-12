75c0e9503eb360a54f915f546ca7a01348abff75
PokeFred <Cedric.paelmke@gmx.de>
2024-03-12 22:54:58 +0100

    init svelte

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite"
+import type { CommonServerOptions } from "vite"
+import { sveltekit } from "@sveltejs/kit/vite"
+import { enhancedImages } from "@sveltejs/enhanced-img"
+
+const serverConfig: CommonServerOptions = {
+    host: "127.0.0.1",
+    port: 3000,
+    strictPort: true
+}
+
+export default defineConfig({
+    server: serverConfig,
+    preview: serverConfig,
+    plugins: [sveltekit(), enhancedImages()]
+})
