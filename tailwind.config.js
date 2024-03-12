556f06c64b7916ebcb352a8dca36cd7ac53f792c
PokeFred <Cedric.paelmke@gmx.de>
2024-03-12 23:33:23 +0100

    add Skeleton UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+import { skeleton } from '@skeletonlabs/tw-plugin'
+import { join as joinPath } from 'path'
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: 'class',
-    content: ['./src/app.html', './src/routes/**/*.svelte', './src/lib/components/**/*.svelte'],
+    content: ['./src/app.html', './src/routes/**/*.svelte', './src/lib/components/**/*.svelte', joinPath(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,svelte,js,ts}')],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        skeleton({
+            themes: {
+                preset: ['skeleton'],
+            },
+        }),
+    ],
 }
