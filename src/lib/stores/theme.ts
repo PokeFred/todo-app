302fa8577f2924b417ca09d9d09b0936613f6b88
PokeFred <Cedric.paelmke@gmx.de>
2024-03-13 14:35:38 +0100

    update

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store'
 import type { Writable } from 'svelte/store'
 
 type Theme = 'skeleton' | 'wintry' | 'modern' | 'seafoam' | 'gold-nouveau'
-const theme: Writable<Theme> = writable<Theme>('wintry')
+const theme: Writable<Theme> = writable<Theme>('skeleton')
 
 export { theme }
 export type { Theme }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass'
 </script>
 
-<div class="container space-y-4 p-10">
+<div class="card space-y-4 p-10">
     <h1>Homepage</h1>
     <hr />
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate nostrum, vero explicabo sunt ipsum asperiores. Molestias nostrum mollitia cumque veniam eum error atque, dolores nesciunt voluptas ea consectetur sed delectus.</p>
